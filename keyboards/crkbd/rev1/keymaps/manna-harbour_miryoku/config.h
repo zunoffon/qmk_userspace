5f257ae9aadf06c03fd518fa9711cf2b44313afe
zun <zunoffon@gmail.com>
2026-06-20 23:08:33 +0700

    cornev4promicroedition: customize miryoku Colemak Mod-DH tap layer with base layer reset

--- keyboards/crkbd/rev1/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/crkbd/rev1/keymaps/manna-harbour_miryoku/config.h
@@ -17,3 +17,27 @@ K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19, \
 K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, \
               K32,   K33,   K34,          K35,   K36,   K37\
 )
+
+/**
+ * @brief Custom tap layer Colemak Mod-DH keymap overrides:
+
+#define MIRYOKU_ALTERNATIVES_TAP_COLEMAKDH \
+KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
+KC_A,              KC_R,              KC_S,              KC_T,              KC_G,              KC_M,              KC_N,              KC_E,              KC_I,              KC_O,              \
+KC_Z,              KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
+
+ *
+ * 1. Replaced KC_DEL with TD(U_TD_U_BASE) on the Tap layer
+ *    Pressing KC_DEL resets the Tap layer back to the Base layer.
+ * @see https://github.com/manna-harbour/miryoku/tree/master/docs/reference#additional-features
+ *
+ */
+#ifdef MIRYOKU_TAP_COLEMAKDH
+#define MIRYOKU_ALTERNATIVES_TAP_COLEMAKDH_CUSTOM \
+KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
+KC_A,              KC_R,              KC_S,              KC_T,              KC_G,              KC_M,              KC_N,              KC_E,              KC_I,              KC_O,              \
+KC_Z,              KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            KC_BSPC,           TD(U_TD_U_BASE),   U_NP,              U_NP
+#define MIRYOKU_LAYER_TAP MIRYOKU_ALTERNATIVES_TAP_COLEMAKDH_CUSTOM
+#endif
