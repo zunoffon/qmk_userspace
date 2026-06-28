b1c3f29aa510a018f963695a12cbffca01a3cc89
zun <zunoffon@gmail.com>
2026-06-28 19:57:01 +0700

    cornev4promicroedition: override default miryoku tap dance behavior for `U_TD_BOOT` layer

--- keyboards/crkbd/rev1/keymaps/miryoku/keymap.c
+++ keyboards/crkbd/rev1/keymaps/miryoku/keymap.c
@@ -7,8 +7,26 @@
 
 // 1/4 - Include header for Miryoku constants (e.g., U_EXTRA, U_BASE)
 #include "manna-harbour_miryoku.h"
+// Mirror declaration, Referenced from: users/miryoku/manna-harbour_miryoku.c, users/miryoku/miryoku_babel/miryoku_layer_list.h
+enum {
+    U_TD_BOOT_CUSTOM,
+#define MIRYOKU_X(LAYER, STRING) U_TD_U_##LAYER##_CUSTOM,
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
 
 // 2/4 - Define custom Tap Dance behavior
+void u_td_fn_boot_custom(tap_dance_state_t *state, void *user_data) {
+  uprintf("%s: Triggered! count: [%d]\n", __func__, state->count);
+  if (state->count == 2) { // Default behavior
+    uprintf("%s: Resetting to bootloader\n", __func__);
+    reset_keyboard();
+  } else if (state->count == 3) {
+    uprintf("%s: Resetting MCU\n", __func__);
+    soft_reset_keyboard();
+  }
+}
+
 void u_td_fn_U_EXTRA_custom(tap_dance_state_t *state, void *user_data) {
   uprintf("%s: Triggered! count: [%d]\n", __func__, state->count);
   if (state->count == 2) { // Default behavior
@@ -31,14 +49,11 @@ extern tap_dance_action_t tap_dance_actions[];
 void keyboard_post_init_user(void) {
   // 4/4 - Hook into post_init to perform the runtime override
   // Create our custom action struct
-  tap_dance_action_t custom_extra_td = ACTION_TAP_DANCE_FN(u_td_fn_U_EXTRA_custom);
-
+  tap_dance_action_t custom_boot_td   = ACTION_TAP_DANCE_FN(u_td_fn_boot_custom);
+  tap_dance_action_t custom_extra_td  = ACTION_TAP_DANCE_FN(u_td_fn_U_EXTRA_custom);
   // Patch the Miryoku action table.
-  // We target index 2 because:
-  // enum { U_TD_BOOT, U_TD_BASE, U_TD_EXTRA, ... }; // U_TD_EXTRA corresponds to index 2
-  // Referenced from: users\miryoku\manna-harbour_miryoku.c, users/miryoku/miryoku_babel/miryoku_layer_list.h
-#define U_TD_U_EXTRA_ENUM_INDEX 2
-  tap_dance_actions[U_TD_U_EXTRA_ENUM_INDEX] = custom_extra_td;
+  tap_dance_actions[U_TD_BOOT_CUSTOM]     = custom_boot_td;
+  tap_dance_actions[U_TD_U_EXTRA_CUSTOM]  = custom_extra_td;
 }
 
 socd_cleaner_t socd_opposing_pairs[] = {
